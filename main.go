82b64265ee484930853b1e8a1d285aca95897577
Abir Biswas <abirbiswas20023010@gmail.com>
2023-04-16 05:23:22 +0530

    next commit

--- main.go
+++ main.go
@@ -4,69 +4,84 @@ import (
     "fmt"
     "net/http"
 
+    "github.com/gorilla/mux"
+    "github.com/gorilla/sessions"
 )
 
-type Config struct {
-	GmailUser     string `json:"gmail-user"`
-	GmailPassword string `json:"gmail-password"`
-}
-
-type User struct {
-	ID    uint   `gorm:"primary_key"`
-	SRFID string `gorm:"unique"`
-	Email string `gorm:"unique"`
-	DOB   string
-}
-
-type HospitalUser struct {
-	ID       uint   `gorm:"primary_key"`
-	HCode    string `gorm:"unique"`
-	Email    string `gorm:"unique"`
-	Password string
-}
-
-type HospitalData struct {
-	ID        uint   `gorm:"primary_key"`
-	HCode     string `gorm:"unique"`
-	HName     string
-	NormalBed int
-	HICUBed   int
-	ICUBed    int
-	VBed      int
-}
-
-type BookingPatient struct {
-	ID       uint   `gorm:"primary_key"`
-	SRFID    string `gorm:"unique"`
-	BedType  string
-	HCode    int
-	SpO2     int
-	PName    string
-	PPhone   string
-	PAddress string
-}
-
-func dbMiddleware() gin.HandlerFunc {
-	db, err := gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/covid?charset=utf8&parseTime=True&loc=Local")
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	db.AutoMigrate(&User{}, &HospitalUser{}, &HospitalData{}, &BookingPatient{})
-
-	return func(c *gin.Context) {
-		c.Set("db", db)
-		c.Next()
-		db.Close()
-	}
-}
+var (
+    // Replace with your own values
+    cookieStore = sessions.NewCookieStore([]byte("super-secret-key"))
+    username    = "user"
+    password    = "password"
+)
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, World!")
-    })
+    r := mux.NewRouter()
+    r.HandleFunc("/", indexHandler)
+    r.HandleFunc("/login", loginHandler).Methods("POST")
+    r.HandleFunc("/logout", logoutHandler).Methods("POST")
 
+    http.ListenAndServe(":8080", r)
+}
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    session, err := cookieStore.Get(r, "session-name")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 
-    http.ListenAndServe(":8080", nil)
+    if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+        http.Redirect(w, r, "/login", http.StatusSeeOther)
+        return
+    }
+
+    fmt.Fprintln(w, "You are logged in.")
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+    username := r.FormValue("username")
+    password := r.FormValue("password")
+
+    if username == "" || password == "" {
+        http.Error(w, "Invalid username or password", http.StatusBadRequest)
+        return
+    }
+
+    if username == username && password == password {
+        session, err := cookieStore.Get(r, "session-name")
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        session.Values["authenticated"] = true
+        err = session.Save(r, w)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        http.Redirect(w, r, "/", http.StatusSeeOther)
+        return
+    }
+
+    http.Error(w, "Invalid username or password", http.StatusBadRequest)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+    session, err := cookieStore.Get(r, "session-name")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    session.Values["authenticated"] = false
+    err = session.Save(r, w)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
