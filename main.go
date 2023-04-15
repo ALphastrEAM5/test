223e70cd27cad244af73073c58a9721cc48eeacf
Abir Biswas <abirbiswas20023010@gmail.com>
2023-04-16 04:44:41 +0530

    next commit

--- main.go
+++ main.go
@@ -9,5 +9,9 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, World!")
     })
+
+    // Set GIN_MODE environment variable to "debug"
+    os.Setenv("GIN_MODE", "debug")
+
     http.ListenAndServe(":8080", nil)
 }
