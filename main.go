38fb91d38cdf9870f63f16d2345c2d00275f5b1d
Abir Biswas <abirbiswas20023010@gmail.com>
2023-04-16 05:02:46 +0530

    next commit

--- main.go
+++ main.go
@@ -3,46 +3,23 @@ package main
 import (
     "fmt"
     "net/http"
+    "encoding/json"
+    "net/smtp"
+    "os"
+
+    "github.com/gin-gonic/gin"
+    "github.com/go-gomail/gomail"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
+    "golang.org/x/crypto/bcrypt"
 )
-
-type User struct {
-	ID    uint   `gorm:"primary_key"`
-	SRFID string `gorm:"unique"`
-	Email string `gorm:"unique"`
-	DOB   string
-}
-
-type HospitalUser struct {
-	ID       uint   `gorm:"primary_key"`
-	HCode    string `gorm:"unique"`
-	Email    string `gorm:"unique"`
-	Password string
-}
-
-type HospitalData struct {
-	ID        uint   `gorm:"primary_key"`
-	HCode     string `gorm:"unique"`
-	HName     string
-	NormalBed int
-	HICUBed   int
-	ICUBed    int
-	VBed      int
-}
-
-type BookingPatient struct {
-	ID       uint   `gorm:"primary_key"`
-	SRFID    string `gorm:"unique"`
-	BedType  string
-	HCode    int
-	SpO2     int
-	PName    string
-	PPhone   string
-	PAddress string
-}
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, World!")
     })
+
+
+
     http.ListenAndServe(":8080", nil)
 }
