1025b3eb9ce96317b3735c11f60e35e996b1365d
Abir Biswas <abirbiswas20023010@gmail.com>
2023-04-16 06:42:55 +0530

    next commit

--- main.go
+++ main.go
@@ -4,27 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
-
 	http.Handle("/", fs)
+
+	http.HandleFunc("/index.html", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
+
 	log.Print("Listening on :8080...")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	f, err := os.Open("./index.html")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	str := "This is a Golang example\nPlease, select a valid repository changing the LAUNCHER_TARGET_REPO value"
-	fmt.Fprintf(w, str)
 }
