20d17b98698ec930f55564857c1f8c980edf183f
Abir Biswas <abirbiswas20023010@gmail.com>
2023-04-16 05:43:43 +0530

    next commit

--- main.go
+++ main.go
@@ -1,87 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-
-    "github.com/gorilla/mux"
-    "github.com/gorilla/sessions"
-)
-
-var (
-    // Replace with your own values
-    cookieStore = sessions.NewCookieStore([]byte("super-secret-key"))
-    username    = "user"
-    password    = "password"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/", indexHandler)
-    r.HandleFunc("/login", loginHandler).Methods("POST")
-    r.HandleFunc("/logout", logoutHandler).Methods("POST")
+	fs := http.FileServer(http.Dir("./static"))
 
-    http.ListenAndServe(":8080", r)
+	http.Handle("/", fs)
+	log.Print("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-    session, err := cookieStore.Get(r, "session-name")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open("./bootstrap.html")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-    if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-        http.Redirect(w, r, "/login", http.StatusSeeOther)
-        return
-    }
-
-    fmt.Fprintln(w, "You are logged in.")
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-    username := r.FormValue("username")
-    password := r.FormValue("password")
-
-    if username == "" || password == "" {
-        http.Error(w, "Invalid username or password", http.StatusBadRequest)
-        return
-    }
-
-    if username == username && password == password {
-        session, err := cookieStore.Get(r, "session-name")
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-
-        session.Values["authenticated"] = true
-        err = session.Save(r, w)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-
-        http.Redirect(w, r, "/", http.StatusSeeOther)
-        return
-    }
-
-    http.Error(w, "Invalid username or password", http.StatusBadRequest)
-}
-
-func logoutHandler(w http.ResponseWriter, r *http.Request) {
-    session, err := cookieStore.Get(r, "session-name")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    session.Values["authenticated"] = false
-    err = session.Save(r, w)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    http.Redirect(w, r, "/login", http.StatusSeeOther)
+	str := "This is a Golang example\nPlease, select a valid repository changing the LAUNCHER_TARGET_REPO value"
+	fmt.Fprintf(w, str)
 }
