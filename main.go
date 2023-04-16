22107cc295bf234c45ec353a0fa097b908a07a01
Abir Biswas <abirbiswas20023010@gmail.com>
2023-04-16 06:57:08 +0530

    next commit

--- main.go
+++ main.go
@@ -17,3 +17,14 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open("./index.html")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	str := "This is a Golang example\nPlease, select a valid repository changing the LAUNCHER_TARGET_REPO value"
+	fmt.Fprintf(w, str)
+}
