327ba2b7d55d9896466714383ad7c0672e9a82dc
Abir Biswas <abirbiswas20023010@gmail.com>
2023-04-16 06:53:42 +0530

    next commit

--- main.go
+++ main.go
@@ -4,18 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
+
 	http.Handle("/", fs)
-
-	http.HandleFunc("/index.html", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/index.html")
-	})
-
-	log.Print("Listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
+	log.Print("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
